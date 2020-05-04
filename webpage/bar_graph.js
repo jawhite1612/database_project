9f5a22ee634b565eea27f96bc504331511f7a11e
Julian White <jwhit139@jhu.edu>
2020-05-04 11:59:32 -0400

    adding party colors

--- webpage/bar_graph.js
+++ webpage/bar_graph.js
@@ -1,23 +1,58 @@
-function createGraph(x, y) {
+function getColor(i) {
+    0;136;0c
+    if (i == 0) {
+	return 0;
+    } else if (i > 0 && i < .5) {
+	return 1;
+    } else if (i == .5) {
+	return 2;
+    } else if (i > .5 && i < 1) {
+	return 3;
+    } else if (i == 1) {
+	return 4;
+    }
+}
+
+function createGraph(x, y, r) {
+    console.log(r);
     var options = {
 		title: {
 			text: "Income"              
 		},
 		data: [              
-		{
+		    {
 			type: "column",
+			color: "red",
 			dataPoints: []
-		}
+		    },
+		    {
+			type: "column",
+			color: "#f57a7a",
+			dataPoints:[]
+		    },
+		     {
+			type: "column",
+			color: "#ede9be",
+			dataPoints:[]
+		     },
+		     {
+			type: "column",
+			color: "#ababff",
+			dataPoints:[]
+		     },
+		     {
+			type: "column",
+			color: "blue",
+			dataPoints:[]
+		    },
 		]
-	};
+    }
     
     for (var i =0; i < x.length; i++) {
 	if (parseInt(y[i]) > 0) {
-
-	    options.data[0].dataPoints.push({label: x[i], y: parseInt(y[i])})
+	    options.data[getColor(r[i])].dataPoints.push({label: x[i], x:i, y: parseInt(y[i])})
 	}
-	}
-
-	$("#chartContainer").CanvasJSChart(options);
+    }
     console.log(options);
+    $("#chartContainer").CanvasJSChart(options);
 }
